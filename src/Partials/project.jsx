7e45afd4c0fa45b341aa6a8b832e993fa961fa96
edenacyg <edenabusiness@gmail.com>
2023-09-07 23:58:03 +0800

    responsive05

--- src/Partials/project.jsx
+++ src/Partials/project.jsx
@@ -5,10 +5,12 @@ function project() {
     return (
         <div className='item'>
             <img src="./images/hero-image.jpg" alt="" />
-            <h2>Project - 001</h2>
-            <div className='icons'>
-                <Circle />
-                <Circle />
+            <div className="project-title">
+                <h2>Project - 001</h2>
+                <div className='icons'>
+                    <Circle />
+                    <Circle />
+                </div>
             </div>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
