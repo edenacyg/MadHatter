836047fedb97fc7e4ef8983704d774942ee32228
edenacyg <edenabusiness@gmail.com>
2023-09-09 23:20:46 +0800

    header menu

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        console.log(isMenuOpen);
+        setIsMenuOpen(!isMenuOpen);
+    };
     return (
         <header>
-            <div className='logo-container'>
-                <h6>The mad hatter</h6>
+            <div className="header-content">
+                <div className='logo-container'>
+                    <h6>The mad hatter</h6>
+                </div>
+
+                <nav className='menu'>
+                    <a className='active' href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Design</a>
+                    <a href="/">Project</a>
+                    <a href="/">Shop</a>
+                </nav>
+
+                <div className="menubar" onClick={toggleMenu}>
+                    <i className="fas fa-bars"></i>
+                </div>
             </div>
-            
-            <nav>
-                <a className='active' href="/" c>Home</a>
+
+            <nav className={`dropdown-menu ${isMenuOpen ? 'open' : ''}`}>
+            <i className="fas fa-times" onClick={toggleMenu}></i>
+                <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/">Design</a>
                 <a href="/">Project</a>
@@ -18,4 +39,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
