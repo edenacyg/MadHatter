9b5fbf12a2d4ab02f8960516a09017489bd7dbb8
edenacyg <edenabusiness@gmail.com>
2023-09-12 21:27:04 +0800

    revision02 - fix mobile responsive

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -5,45 +5,47 @@ function Footer() {
     return (
         <footer>
             <div className="footer-con">
-                <div className="categories">
-                    <Menu
-                        title="The Company"
-                        menus={[
-                            { label: "New Designs" },
-                            { label: "Weekly Insights" },
-                            { label: "Collaborations" },
-                        ]}
-                    />
+                <div className="footer-subcon">
+                    <div className="categories">
+                        <Menu
+                            title="The Company"
+                            menus={[
+                                { label: "New Designs" },
+                                { label: "Weekly Insights" },
+                                { label: "Collaborations" },
+                            ]}
+                        />
 
-                    <Menu
-                        title="The Team"
-                        menus={[
-                            { label: "Meet the Team" },
-                            { label: "Our Designers" },
-                            { label: "The Principles" },
-                        ]}
-                    />
+                        <Menu
+                            title="The Team"
+                            menus={[
+                                { label: "Meet the Team" },
+                                { label: "Our Designers" },
+                                { label: "The Principles" },
+                            ]}
+                        />
 
-                    <Menu
-                        title="Our Design"
-                        menus={[
-                            { label: "Intentional Research" },
-                            { label: "Digital Solutions" },
-                            { label: "Innovations" },
-                        ]}
-                    />
+                        <Menu
+                            title="Our Design"
+                            menus={[
+                                { label: "Intentional Research" },
+                                { label: "Digital Solutions" },
+                                { label: "Innovations" },
+                            ]}
+                        />
 
-                    <Menu
-                        title="Legal"
-                        menus={[
-                            { label: "Terms and Confitions" },
-                            { label: "Privacy Policy" },
-                            { label: "Copyright" },
-                        ]}
-                    />
-                </div>
-                <div>
-                    <p>Copyright 2021 Fashion with Ethics All RIghts Reserved.</p>
+                        <Menu
+                            title="Legal"
+                            menus={[
+                                { label: "Terms and Confitions" },
+                                { label: "Privacy Policy" },
+                                { label: "Copyright" },
+                            ]}
+                        />
+                    </div>
+                    <div>
+                        <p>Copyright 2021 Fashion with Ethics All RIghts Reserved.</p>
+                    </div>
                 </div>
             </div>
         </footer>
